eca7de65a5d1f84dd6936efd55e626d2fbe72fa5
Heba Elbeshbeshy <heba.elbeshbeshy12@eng-st.cu.edu.eg>
2021-06-05 16:33:54 +0200

    Eldonya nawrtt Elhamdullaah

--- Main.py
+++ Main.py
@@ -1,15 +1,10 @@
 import sys
 import os
-# import vtkmodules.qt
-# vtkmodules.qt.QVTKRWIBase = 'QGLWidget' 
 import vtk
-# import vtk.qt
-# vtk.qt.QVTKRWIBase = "QGLWidget"
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import *
 import pydicom
 import glob
-# from PyQt5.QtWidgets import QMainWindow, QApplication
 from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 from partB import Ui_MainWindow
 
@@ -20,8 +15,7 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        
-       
+    
        # ##############################################################
         self.Slider =[self.ui.verticalSlider , self.ui.verticalSlider_2]
         ##############################################################
@@ -33,16 +27,16 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.LoadDICOM.clicked.connect(lambda: self.Load_DICOM_Series())
         # self.ui.comboBox.activated.connect()
         # ##############################################################
-        
-
+        self.iren = QVTKRenderWindowInteractor()
         self.surfaceExtractor = vtk.vtkContourFilter()
 
     def DICOM_SERIES_PATH(self):
         # shyla path el folder nafso 
-        path = QFileDialog.getExistingDirectory(self, 'Choose DICOM Directory')
+        path = QFileDialog.getExistingDirectory(self, 'Choose DICOM Directory') + '/'
         # f de array shayl kol el paths bta3t el swar ely gwa el folder
         f = glob.glob(rf'{path}/*.dcm')
         # slices = [pydicom.read_file(s, force=True) for s in f] // baianaat om el patient w elsora bd5loni f tafsel malesh da3wa beha leh bgdd :"
+        print(path)
         return path 
     
 
@@ -50,31 +44,21 @@ class MainWindow(QtWidgets.QMainWindow):
         val = self.Slider[ID].value()
         # print(val, ID)
         self.surfaceExtractor.SetValue(0, val)
-        # iren.update()
-        # self.reader.Update()
+        self.iren.update()
             
     def Load_DICOM_Series(self):
         print("H")
         # self.ui.openGLWidget_2
+        viewer = self.iren.GetRenderWindow()
         aRenderer = vtk.vtkRenderer()
-        iren = vtk.vtkRenderWindowInteractor()
-        # self.frame = QtWidgets.QFrame()
-        self.vl = self.ui.graphicsView
-        # iren = QVTKRenderWindowInteractor(self.vl)
-        # self.vl.addWidget(iren)
-        
+        viewer.AddRenderer(aRenderer)
+
+        # Read Dataset using vtkDICOMImageReader
         pathDicomDir = self.DICOM_SERIES_PATH()
+        print(pathDicomDir)
         self.reader = vtk.vtkDICOMImageReader()
         self.reader.SetDirectoryName(pathDicomDir)
-       
-        self.reader.SetDataByteOrderToLittleEndian()
-        self.reader.SetDataSpacing(3.2, 3.2, 1.5)
         self.reader.Update()
-
-        # self.viewer = vtk.vtkImageViewer()
-        # self.viewer = vtk.vtkImageViewer2()
-        self.viewer = vtk.vtkResliceImageViewer()
-        self.viewer.SetInputData(self.reader.GetOutput())
 
         self.surfaceExtractor.SetInputConnection(self.reader.GetOutputPort())
         self.surfaceExtractor.SetValue(0, 500)
@@ -89,13 +73,26 @@ class MainWindow(QtWidgets.QMainWindow):
 
         surface = vtk.vtkActor()
         surface.SetMapper(self.surfaceMapper)
+       
+        aCamera = vtk.vtkCamera()
+        aCamera.SetViewUp(0, 0, -1)
+        aCamera.SetPosition(0, 1, 0)
+        aCamera.SetFocalPoint(0, 0, 0)
+        aCamera.ComputeViewPlaneNormal()
 
         aRenderer.AddActor(surface)
-        self.viewer.SetupInteractor(iren)
-       
-        self.viewer.SetRenderWindow(iren.GetRenderWindow())
-        self.viewer.Render()
-       
+        aRenderer.SetActiveCamera(aCamera)
+        aRenderer.ResetCamera()
+        aCamera.Dolly(1)
+
+        aRenderer.SetBackground(0, 0, 0)
+        viewer.SetSize(640, 480)
+        aRenderer.ResetCameraClippingRange()
+
+        self.iren.Initialize()
+        viewer.Render()
+        self.iren.Start()
+        self.iren.show()
         print("E")
 
 
