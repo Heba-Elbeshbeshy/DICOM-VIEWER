8afdadd34b4510a54635400ab91002c5fa317c4d
Heba Elbeshbeshy <heba.elbeshbeshy12@eng-st.cu.edu.eg>
2021-06-08 02:09:49 +0200

    DONEEE

--- Main.py
+++ Main.py
@@ -3,11 +3,9 @@ import os
 import vtk
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import *
-import pydicom
-import glob
 from vtk.util.misc import vtkGetDataRoot
 from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
-from partBb import Ui_MainWindow
+from UI import Ui_MainWindow
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -17,32 +15,35 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
 
         ##############################################################
-        self.Slider = [self.ui.horizontalSlider , self.ui.horizontalSlider_2 , self.ui.horizontalSlider_3 , self.ui.horizontalSlider_4]
-        self.ui.horizontalSlider.valueChanged.connect(lambda: self.slider_SLOT(0))
-        self.ui.horizontalSlider_2.valueChanged.connect(lambda: self.slider_SLOT(1))
-        self.ui.horizontalSlider_3.valueChanged.connect(lambda: self.slider_SLOT(2))
-        self.ui.horizontalSlider_4.valueChanged.connect(lambda: self.slider_SLOT(3))
+        self.Slider = [self.ui.horizontalSlider , self.ui.horizontalSlider_2 , self.ui.horizontalSlider_3 , self.ui.horizontalSlider_4 , self.ui.TissueSlider] 
+        self.ui.horizontalSlider.valueChanged.connect(lambda: self.Slider_Changed(0))
+        self.ui.horizontalSlider_2.valueChanged.connect(lambda: self.Slider_Changed(1))
+        self.ui.horizontalSlider_3.valueChanged.connect(lambda: self.Slider_Changed(2))
+        self.ui.horizontalSlider_4.valueChanged.connect(lambda: self.Slider_Changed(3))
+        self.ui.TissueSlider.valueChanged.connect(lambda: self.Slider_Changed(4))
         self.ui.LoadDICOM.clicked.connect(lambda: self.Load_DICOM_Series())
         self.ui.comboBox.activated.connect(lambda: self.Rendering_Mode())
         # ##############################################################
         
         self.surfaceExtractor = vtk.vtkContourFilter()
+        self.volumeColor = vtk.vtkColorTransferFunction()
+
         self.RenMD = 0
         self.Flag = False
-        self.SliderChanged = False
-        self.Edit_UI(0)
 
+        self.Edit_UI(0)
+    
+    ##########################################
     def DICOM_SERIES_PATH(self):
         path = QFileDialog.getExistingDirectory(self, 'Choose DICOM Directory') + '/'
         self.Name = path.split('/')[-2]
         return path 
-                
-    def Load_DICOM_Series(self):
-        print("H")
-        self.Flag = True   
 
-        # Read Dataset using vtkDICOMImageReader
+    ##########################################   
+    def Load_DICOM_Series(self):
+        self.Flag = True   
         pathDicomDir = None  
+
         pathDicomDir = self.DICOM_SERIES_PATH()
         self.reader = vtk.vtkDICOMImageReader()
         self.reader.SetDirectoryName(pathDicomDir)
@@ -52,96 +53,63 @@ class MainWindow(QtWidgets.QMainWindow):
             self.ui.label_9.setText(self.Name)
             self.Rendering_Mode()
 
+    ##########################################
     def Rendering_Mode(self):
         self.RenMD = self.ui.comboBox.currentIndex() 
 
         if self.RenMD == 1:
             self.Edit_UI(self.RenMD)
             self.Surface_Rendering()
-
         elif self.RenMD == 2:
             self.Edit_UI(self.RenMD)
             self.Ray_Casting_Rendering()
     
-    def slider_SLOT(self , ID):
-        val = self.Slider[ID].value()
-
+    ##########################################
+    def Slider_Changed(self , ID):
+        iren = QVTKRenderWindowInteractor()
+        self.Tspt = self.ui.TissueSlider.value() #Tissue Point Changed
+        self.ui.label_11.setText(str(self.Tspt))
+        
+        # Iso_Value
         if ID == 0:
-            self.iren = QVTKRenderWindowInteractor()
+            val = self.Slider[ID].value()
             self.surfaceExtractor.SetValue(0, val)
-            self.iren.update()
-
+        
+        # Transfer_Func.    
         elif ID == 1:
-            val = val/100.0
-            self.SliderChanged = True
-            print("Heba")
-            print(val)
-            self.iren = QVTKRenderWindowInteractor()
-            volumecolor = vtk.vtkColorTransferFunction()
-            # volumecolor.GetRedValue(val)
-            # RemoveAllPoints()
-            volumecolor.AddRGBPoint(0,    0.0, 0.0, 0.0)
-            volumecolor.AddRGBPoint(500,  val, 0.5, 0.3)
-            volumecolor.AddRGBPoint(1000, val, 0.5, 0.3)
-            volumecolor.AddRGBPoint(1150, val, 1.0, 0.9) 
+            val = self.Slider[ID].value()/100.0
+            self.ui.label_6.setText(str(val))
 
-            volumeProperty = vtk.vtkVolumeProperty()
-            volumeProperty.SetColor(volumecolor)
-            volume = vtk.vtkVolume()
-            volume.SetProperty(volumeProperty)
-            self.iren.update() 
-            self.Ray_Casting_Rendering()
-            print("T")
-            
+            GVal=self.volumeColor.GetGreenValue(self.Tspt) 
+            BVal=self.volumeColor.GetBlueValue(self.Tspt)
+            self.volumeColor.RemovePoint(self.Tspt)
+            self.volumeColor.AddRGBPoint(self.Tspt,  val, GVal , BVal)
+               
         elif ID == 2:
-            val = val/100.0
-            print("Hebaaa")
-            self.SliderChanged = True
-            print(val)
-            self.iren = QVTKRenderWindowInteractor()
-            volumecolor = vtk.vtkColorTransferFunction()
-            # volumecolor.GetGreenValue(val)
-            # # RemoveAllPoints()
-            volumecolor.AddRGBPoint(0,    0.0, 0.0, 0.0)
-            volumecolor.AddRGBPoint(500,  1.0, val, 0.3)
-            volumecolor.AddRGBPoint(1000, 1.0, val, 0.3)
-            volumecolor.AddRGBPoint(1150, 1.0, 1.0, 0.9) 
+            val = self.Slider[ID].value()/100.0
+            self.ui.label_7.setText(str(val))
+        
+            RVal=self.volumeColor.GetRedValue(self.Tspt)
+            BVal=self.volumeColor.GetBlueValue(self.Tspt)
+            self.volumeColor.RemovePoint(self.Tspt)
+            self.volumeColor.AddRGBPoint(self.Tspt,  RVal, val , BVal)
 
-            volumeProperty = vtk.vtkVolumeProperty()
-            volumeProperty.SetColor(volumecolor)
-            volume = vtk.vtkVolume()
-            volume.SetProperty(volumeProperty)
-            self.iren.update()
-            # self.Ray_Casting_Rendering()
-            print("k")
-            
         elif ID == 3:
-            val = val/100.0
-            print("Bebo")
-            self.SliderChanged = True
-            print(val)
-            self.iren = QVTKRenderWindowInteractor()
-            volumecolor = vtk.vtkColorTransferFunction()
-            # volumecolor.GetBlueValue (val)
-            volumecolor.AddRGBPoint(0,    0.0, 0.0, 0.0)
-            volumecolor.AddRGBPoint(500,  1.0, 0.5, val)
-            volumecolor.AddRGBPoint(1000, 1.0, 0.5, val)
-            volumecolor.AddRGBPoint(1150, 1.0, 1.0, 0.9)
-            volumeProperty = vtk.vtkVolumeProperty()
-            volumeProperty.SetColor(volumecolor)
-            volume = vtk.vtkVolume()
-            volume.SetProperty(volumeProperty)
-            self.iren.update()
-            # self.Ray_Casting_Rendering()
-            print("A")
+            val = self.Slider[ID].value()/100.0
+            self.ui.label_8.setText(str(val))
             
+            RVal=self.volumeColor.GetRedValue(self.Tspt)
+            GVal=self.volumeColor.GetGreenValue(self.Tspt) 
+            self.volumeColor.RemovePoint(self.Tspt)
+            self.volumeColor.AddRGBPoint(self.Tspt,  RVal, GVal , val)
 
-    #  Surface_Rendering Mode
+        iren.update()
+
+    ##########################################            
     def Surface_Rendering(self):
-        print("B")
         if self.Flag == True : 
-            self.iren = QVTKRenderWindowInteractor()
-            self.viewer = self.iren.GetRenderWindow()
+            iren = QVTKRenderWindowInteractor()
+            self.viewer = iren.GetRenderWindow()
             self.aRenderer = vtk.vtkRenderer()
             self.viewer.AddRenderer(self.aRenderer) 
 
@@ -172,51 +140,43 @@ class MainWindow(QtWidgets.QMainWindow):
             self.viewer.SetSize(640, 480)
             self.aRenderer.ResetCameraClippingRange()
 
-            self.iren.Initialize()
+            iren.Initialize()
             self.viewer.Render()
-            self.iren.Start()
-            self.iren.show()
-            print("E")
-        
-    # Ray Casting Rendering Mode
+            iren.Start()
+            iren.show()
+            
+    ##########################################
     def Ray_Casting_Rendering(self):
-        if self.Flag == True and  self.SliderChanged == False:
 
+        if self.Flag == True:
             iren = QVTKRenderWindowInteractor()
             renWin = iren.GetRenderWindow()
             ren = vtk.vtkRenderer()
             renWin.AddRenderer(ren) 
 
-            # # ren = vtk.vtkRenderer()
-            # renWin = vtk.vtkRenderWindow()
-            # renWin.AddRenderer(ren)
-            # iren = vtk.vtkRenderWindowInteractor()
-            # iren.SetRenderWindow(renWin)
-
             volumeMapper = vtk.vtkGPUVolumeRayCastMapper()
             volumeMapper.SetInputConnection(self.reader.GetOutputPort())
             volumeMapper.SetBlendModeToComposite()
 
-            volumeColor = vtk.vtkColorTransferFunction()
-            volumeColor.AddRGBPoint(0,    0.0, 0.0, 0.0)
-            volumeColor.AddRGBPoint(500,  1.0, 0.5, 0.3)
-            volumeColor.AddRGBPoint(1000, 1.0, 0.5, 0.3)
-            volumeColor.AddRGBPoint(1150, 1.0, 1.0, 0.9)
-
+            self.volumeColor = vtk.vtkColorTransferFunction()
+            self.volumeColor.AddRGBPoint(0,    0.0, 0.0, 0.0)
+            self.volumeColor.AddRGBPoint(500,  1.0, 0.5, 0.3)
+            self.volumeColor.AddRGBPoint(1000,  1.0, 0.5, 0.3)
+            self.volumeColor.AddRGBPoint(1150, 1.0, 1.0, 0.9)
+        
             volumeScalarOpacity = vtk.vtkPiecewiseFunction()
             volumeScalarOpacity.AddPoint(0,    0.00)
-            volumeScalarOpacity.AddPoint(500,  0.15)
-            volumeScalarOpacity.AddPoint(1000, 0.15)
-            volumeScalarOpacity.AddPoint(1150, 0.85)
-
+            volumeScalarOpacity.AddPoint(500,  1.0)
+            volumeScalarOpacity.AddPoint(1000, 0.7)
+            volumeScalarOpacity.AddPoint(1150, 0.03)
+        
             volumeGradientOpacity = vtk.vtkPiecewiseFunction()
             volumeGradientOpacity.AddPoint(0,   0.0)
             volumeGradientOpacity.AddPoint(90,  0.5)
             volumeGradientOpacity.AddPoint(100, 1.0)
 
             volumeProperty = vtk.vtkVolumeProperty()
-            if self.SliderChanged == False:
-                volumeProperty.SetColor(volumeColor)
+            volumeProperty.SetColor(self.volumeColor)
             volumeProperty.SetScalarOpacity(volumeScalarOpacity)
             volumeProperty.SetGradientOpacity(volumeGradientOpacity)
             volumeProperty.SetInterpolationTypeToLinear()
@@ -230,11 +190,11 @@ class MainWindow(QtWidgets.QMainWindow):
             volume.SetProperty(volumeProperty)
 
             ren.AddViewProp(volume)
-            
+
             camera =  ren.GetActiveCamera()
             c = volume.GetCenter()
             camera.SetFocalPoint(c[0], c[1], c[2])
-            camera.SetPosition(c[0] + 500, c[1], c[2])
+            camera.SetPosition(c[0] + 600, c[1], c[2])
             camera.SetViewUp(0, 0, -1)
 
             renWin.SetSize(640, 480)
@@ -244,13 +204,14 @@ class MainWindow(QtWidgets.QMainWindow):
             iren.Start()
             iren.show()
 
-    
+    ##########################################
     def Edit_UI (self , RenMD):
         if RenMD == 0:
             self.ui.horizontalSlider.hide()
             self.ui.horizontalSlider_2.hide()
             self.ui.horizontalSlider_3.hide()
             self.ui.horizontalSlider_4.hide()
+            self.ui.TissueSlider.hide()
             self.ui.label.hide()
             self.ui.label_2.hide()
             self.ui.label_3.hide()
@@ -259,13 +220,16 @@ class MainWindow(QtWidgets.QMainWindow):
             self.ui.label_6.hide()
             self.ui.label_7.hide()
             self.ui.label_8.hide()
-            MainWindow.resize(self, 529, 100)
+            self.ui.label_11.hide()
+            self.ui.TissueLabel.hide()
+            MainWindow.resize(self, 529, 115)
 
         if RenMD == 1:
             self.ui.horizontalSlider.show()
             self.ui.horizontalSlider_2.hide()
             self.ui.horizontalSlider_3.hide()
             self.ui.horizontalSlider_4.hide()
+            self.ui.TissueSlider.hide()
             self.ui.label.show()
             self.ui.label_2.show()
             self.ui.label_3.hide()
@@ -274,13 +238,16 @@ class MainWindow(QtWidgets.QMainWindow):
             self.ui.label_6.hide()
             self.ui.label_7.hide()
             self.ui.label_8.hide()
-            MainWindow.resize(self, 529, 250)
+            self.ui.label_11.hide()
+            self.ui.TissueLabel.hide()
+            MainWindow.resize(self, 529, 230)
 
         elif RenMD== 2:
             self.ui.horizontalSlider.hide()
             self.ui.horizontalSlider_2.show()
             self.ui.horizontalSlider_3.show()
             self.ui.horizontalSlider_4.show()
+            self.ui.TissueSlider.show()
             self.ui.label.hide()
             self.ui.label_2.hide()
             self.ui.label_3.show()
@@ -289,7 +256,9 @@ class MainWindow(QtWidgets.QMainWindow):
             self.ui.label_6.show()
             self.ui.label_7.show()
             self.ui.label_8.show()
-            MainWindow.resize(self, 529, 500)
+            self.ui.label_11.show()
+            self.ui.TissueLabel.show()
+            MainWindow.resize(self, 529, 490)
 
 def main():
     path = os.getcwd()
@@ -302,10 +271,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-   
-
-    
- # if(Heba != 0):
-        #     self.viewer.Finalize()
-        #     self.iren.TerminateApp()
-        #     del self.viewer, self.iren
